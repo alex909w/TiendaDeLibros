ca908baae301e6bd76a5dcf3cdc90d15b94f1eab
JeffersonVa16 <velascojefferson234@gmail.com>
2025-02-03 12:52:27 -0600

    Jefferson

---
+++ js/carrito.js
@@ -0,0 +1,38 @@
+const productos = [
+    { id: 1, nombre: "El Principito", precio: 10, stock: 10, imagen: "img/ElPrincipito.jpg" },
+    { id: 2, nombre: "Harry Potter y la Piedra Filosofal", precio: 15, stock: 8, imagen:
+   "img/harry.webp" } ];
+
+   let carrito = [];
+   function agregarAlCarrito(nombre, precio, idCantidad, idCantidadSeleccionada) {
+    let cantidadSeleccionada =
+   parseInt(document.getElementById(idCantidadSeleccionada).value);
+    let cantidadDisponible = parseInt(document.getElementById(idCantidad).textContent);
+    if (cantidadSeleccionada > cantidadDisponible) {
+    cantidadSeleccionada = cantidadDisponible;
+    }
+
+    let productoExistente = carrito.find(item => item.nombre === nombre);
+    if (productoExistente) {
+    productoExistente.cantidad += cantidadSeleccionada;
+    } else {
+    const producto = productos.find(p => p.nombre === nombre);
+    carrito.push({
+    nombre: nombre,
+    precio: precio,
+    cantidad: cantidadSeleccionada,
+    imagen: producto.imagen
+    });
+    }
+
+    const producto = productos.find(p => p.nombre === nombre);
+    producto.stock -= cantidadSeleccionada;
+    document.getElementById(idCantidad).textContent = producto.stock;
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    actualizarContadorCarrito();
+   }
+
+   function actualizarContadorCarrito() {
+    let contador = carrito.reduce((total, item) => total + item.cantidad, 0);
+    document.getElementById("contador-carrito").textContent = contador;
+   }
