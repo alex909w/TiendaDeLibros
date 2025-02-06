cf8aec1ad3b88d4fc1734319f65b059b62ad9c36
JeffersonVa16 <velascojefferson234@gmail.com>
2025-02-05 20:42:34 -0600

    Modificación

--- js/carrito.js
+++ js/carrito.js
@@ -3,6 +3,8 @@ const productos = [
     { id: 2, nombre: "Harry Potter y la Piedra Filosofal", precio: 15, stock: 8, imagen: "img/harry.webp" }, 
     { id: 3, nombre: "Cien años de soledad", precio: 22, stock: 7, imagen: "img/CienAñosDeSoledad.jpg" },
     { id: 4, nombre: "1984 - George Orwell", precio: 18, stock: 10, imagen: "img/1984.jpg" },
+    { id: 5, nombre: "Orgullo y Prejuicio", precio: 14, stock: 5, imagen: "img/OrgulloYPrejuicio.jpg" },
+    { id: 6, nombre: "El Señor de los Anillos", precio: 30, stock: 6, imagen: "img/ElSeñorDeLosAnillos.jpg" },
 ];
 
    let carrito = [];
